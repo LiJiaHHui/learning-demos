b494810916e461468d6122002328cf25156ab0e2
LiJiaHHui <1151329119@qq.com>
2021-12-14 16:28:14 +0800

    feat: "ui细节调整+useState使用"

--- react/1/src/App.js
+++ react/1/src/App.js
@@ -1,23 +1,28 @@
 import './App.css';
-// import { useEffect,useState  } from 'react';
+import { useEffect,useState  } from 'react';
 const picMap=[
   'https://lijiahhui.github.io/image/%E6%9D%BE%E9%9B%80.jpg',
   'https://lijiahhui.github.io/image/%E7%81%AB%E7%84%B0%E9%B8%9F.jpg',
   'https://lijiahhui.github.io/image/%E7%81%AB%E5%B1%B1%E5%8F%A3%E6%B9%96.jpg',
-  'https://lijiahhui.github.io/image/%E6%9D%BE%E9%9B%80.jpg',
-  'https://lijiahhui.github.io/image/%E7%81%AB%E7%84%B0%E9%B8%9F.jpg',
+  'https://lijiahhui.github.io/image/%E5%B0%8F%E5%B1%8B.jpg',
+  'https://lijiahhui.github.io/image/%E5%B1%82%E6%9E%97%E6%B5%B8%E6%9F%93.jpg',
 ]
   
 
 function App() {
+  // 与v-model的区别
+  // 大概是双向数据绑定与单向
+  const [initData,setInitData]=useState(0) 
+  const changeHandle=(item)=>setInitData(item)
   return (
     <div className="App">
       {
         picMap.map((item,ind)=>(
           <div
-            className={`pic ${index}===`}
+            className={`pic ${ind===initData?'active':''}`}
             style={{background:`url(${item})`}}
             key={ item + ind}
+            onClick={changeHandle.bind(this,ind)}
           >
           </div> 
         ))
