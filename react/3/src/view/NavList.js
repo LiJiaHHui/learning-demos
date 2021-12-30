6d194189ac1cb9362e18db157b82654f386ea8a0
LiJiaHHui <1151329119@qq.com>
2021-12-30 17:28:13 +0800

    feat: “添加项目”

---
+++ react/3/src/view/NavList.js
@@ -0,0 +1,14 @@
+import {Component} from 'react';
+class NavList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (
+      <div></div>
+      );
+  }
+}
+ 
+export default NavList;
---
+++ react/3/src/view/page.js
@@ -0,0 +1,3 @@
+export {default as Content} from './content'
+export {default as NavList} from './NavList' 
+export {default as LeftMenu} from './LeftMenu' 
