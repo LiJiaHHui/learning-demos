37868bf02935c51c42f4693b78d36a9eb0c6b9e3
LiJiaHHui <1151329119@qq.com>
2022-01-10 17:27:27 +0800

    feat: "新增项目&&添加input"

---
+++ react/4/src/component/input.js
@@ -0,0 +1,15 @@
+// class Input extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {  }
+//   }
+//   render() { 
+//     return (  );
+//   }
+// }
+// 函数组件
+const Input=()=>{
+  return render (){
+  }
+}
+export default Input;
